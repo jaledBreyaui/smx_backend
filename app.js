318726492c2957163c504ff08811f50752d7a825
Jaled <jaledbreyaui41@gmail.com>
2025-02-16 16:16:57 -0300

    prod-setup

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const chatRoutes = require('./src/routes/apiCall')
 
 app.use(cors(
     {
-        origin: ["https://jaledbreyaui.github.io/smx_frontend/", 'http://127.0.0.1:5173', "http://localhost:5173"],
+        origin: ["https://jaledbreyaui.github.io", 'http://127.0.0.1:5173', "http://localhost:5173"],
         credentials: true,
         methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD", "DELETE"],
         allowedHeaders: ['Content-Type', 'Authorization']
