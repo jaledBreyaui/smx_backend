850a6b21bc61b334c88634180b805ab5faa9910e
Jaled <jaledbreyaui41@gmail.com>
2025-02-18 01:21:40 -0300

    model improvement v2

--- src/services/gptApiCall.js
+++ src/services/gptApiCall.js
@@ -42,7 +42,7 @@ async function callOpenAI(prompt) {
                                 properties: {
                                     ready: {
                                         type: "boolean", description: `true si el usuario está listo para contacto. preguntale para confirmar "Puedo hacer que un representante se comunique contigo si asi lo deseas"
-                                    -Si notas que el usuario está listo para hablar con un representante, activa la función 'setReadyToAction' con { ready: true }. espera por lo menos que te haga 2 0 3 preguntas para recien activar esta funcionalidad.
+                                    -Si notas que el usuario está listo para hablar con un representante, activa la función 'setReadyToAction' con { ready: true }.
                                     -Algunos indicios de interés incluyen: preguntar por precios, disponibilidad, agendar una reunión, formas de contacto, o mencionar que quieren hablar con alguien.
                                     -Sin embargo, no actives la función si el usuario solo está explorando o tiene dudas generales.
                                     -Siempre responde con un mensaje útil antes de activar la función.` }
